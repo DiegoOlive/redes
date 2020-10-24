c215a83af07a2e211aa15da065c2047306f300b2
Your Name <you@example.com>
2020-10-24 09:54:05 -0300

    consegui terminar o banner aula 16

--- src/components/header/banner/index.tsx
+++ src/components/header/banner/index.tsx
@@ -2,13 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 import Img from '../../../assets/images/home/inicio.png';
 import BannerImg from '../bannerImg/';
-import BannerMessager from '../bannerMessager';
+import BannerMessager, {MessageContainer} from '../bannerMessager';
 
 const BannerContainer = styled.div`
     display: flex;
     margin: 10px;  
     flex-direction: column;
-    align-items:center;         
+    align-items:center;
+        
+       
+    @media (min-width: 768px){
+        display:grid;
+        grid-template-columns: 3fr 2fr;
+        grid-template-areas:
+            "message figure";
+    
+        & ${BannerImg}{
+            grid-area: figure;
+            justify-self: end;
+            margin-right: 100px;   
+        }
+
+        & ${MessageContainer}{
+            grid-area: message;
+        }        
+    }
   `
 
 export default function Banner(){
--- src/components/header/bannerMessager/index.tsx
+++ src/components/header/bannerMessager/index.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const MessageContainer = styled.div`
+export const MessageContainer = styled.div`
     width: 70%;
     color: var(--branco);
     font-family: 'Roboto', sans-serif;
     text-align: center;
     margin-top: 10px;
     margin-bottom: 10px;
-    line-height: 1.4;
+    line-height: 1.6;
+
+    @media (min-width: 768px){
+        width: 900px;
+        display: flex;
+        flex-direction: column;
+        text-align: start;
+        margin-left: 100px;
         
 `
 
 //rem valendo 10
 const Title =styled.h1`
     font-size: 2.4rem;
+
+    @media (min-width: 768px){
+        font-size: 3.1rem;
+    }
 `
 
 const Description = styled.h2`
     font-weight: normal;
+    font-size: 1.8rem;
     margin: 10px 0;
 `
 
@@ -31,8 +43,7 @@ export default function Message(){
 
             <Description>
                 Fique por dentro dos melhores conteúdos relacionados a redes de computadores.
-            </Description>
-            
+            </Description>            
         </MessageContainer>
     )
 }
