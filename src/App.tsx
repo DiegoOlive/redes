150c3843c3c87a4dd2b54d4584630362ddc4c963
Your Name <you@example.com>
2020-12-04 18:58:09 -0300

    ajustes no front - conexão com o back

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 //semelhante ao main da aplicação
-import React from 'react';
-import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import {BrowserRouter,Switch,Route, Redirect} from 'react-router-dom';
 import Home from './pages/home';
 import Erro404 from './pages/erro404';
 import Topologia from './pages/topologia';
@@ -8,6 +8,19 @@ import Historia from './pages/historia';
 import CriarConta from './pages/criarConta';
 import EditarConta from './pages/editarConta';
 import Login from './pages/login';
+import IsAuthenticated from './auth';
+
+/*const PrivateRouter ({component: Component, ...rest}) =>(
+  <Route {...rest} render={props => (
+    IsAuthenticated () ?(
+      <Component {...props} />
+    ) :(
+      <Redirect to ={{path= "/", state. {from: props.location}}}/>
+    )
+  }
+  />
+);*/
+
 
 function App() {
   return (
